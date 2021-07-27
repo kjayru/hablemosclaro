7146b875da0e601e44531685b39e26f2cb992283
Wile <desarrollo@miwebprofesional.com>
2021-07-27 12:42:56 -0500

    fix

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -124,7 +124,7 @@ class HomeController extends Controller
             }
             $articulos = collect($post);
 
-            dd($articulos);
+
         }else{
             $categorias = $category->posts;
 
