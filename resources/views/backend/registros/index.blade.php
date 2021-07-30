631a692734f49608182a16b816246de11fb028cd
Wile <desarrollo@miwebprofesional.com>
2021-07-30 11:39:46 -0500

    fix

--- resources/views/backend/registros/index.blade.php
+++ resources/views/backend/registros/index.blade.php
@@ -61,7 +61,7 @@
                         <th>{{ $key+1}}</th>
                         <td>{{ @$reg->email}}</td>
                         <td>{{ @$reg->temas}}</td>
-                        <td>{{ @$reg->created_at)->format('d/m/Y') }} </td>
+                        <td>{{ Carbon\Carbon::parse(@$reg->created_at)->format('d/m/Y') }} </td>
 
                     </tr>
                     @endforeach
