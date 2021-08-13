1e61cbc9f9d67521b5437c0742e575e85aa108b4
jhonatanwqr90 <jhonatanw.quisperojas@gmail.com>
2021-08-13 14:39:20 -0500

    Implemenetación de GTM

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -49,10 +49,31 @@
         <link rel="shortcut icon" href="/favicon.ico" />
         <link id="site-css" rel="stylesheet" href="/assets/public/css/site.css?v={{uniqid()}}">
 
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-W4FSDQW');</script>
+        <!-- End Google Tag Manager -->
 
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-C6X7J05FFR"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-C6X7J05FFR');
+        </script>
 
     </head>
     <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W4FSDQW"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+
 
 @include('layouts.frontend.partials.header')
 
