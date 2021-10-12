f17a046163b9a3d5d067dd5f03172189525030dc
jhonatanwqr90 <jhonatanw.quisperojas@gmail.com>
2021-10-12 10:47:21 -0500

    Maquetación de error 404, implementé la maquetación en el home

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -85,9 +85,17 @@
    @include('layouts.frontend.partials.columna')
    @include('layouts.frontend.partials.video')
 
-   <section class="section error">
-       <article class="error__content">
-           
+   <section class="error">
+       <article class="limit">
+           <h4 class="error__title">
+               Error
+               <strong>404</strong>
+           </h4>
+           <strong class="error__subtitle">Ups! No pudimos encontrar esa página</strong>
+           <p class="error__text">Regresa a la página de inicio</p>
+           <div class="g-button-group">
+               <a href="/" class="g-button m--mini m--211 m--rojo">Ir a inicio</a>
+           </div>
        </article>
    </section>
 
