a4e8aaa1e5b7551ab48de5350bb5c5773627e53a
Wile <desarrollo@miwebprofesional.com>
2021-10-12 13:47:06 -0500

    implementacion de errores

---
+++ resources/views/errors/401.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.frontend.app')
+@section('content')
+
+      <section class="error">
+       <article class="limit">
+           <h4 class="error__title">
+               Error
+               <strong>401</strong>
+           </h4>
+           <strong class="error__subtitle">Ups! no estás autorizado</strong>
+           <p class="error__text">Regresa a la página de inicio</p>
+           <div class="g-button-group">
+               <a href="/" class="g-button m--mini m--211 m--rojo">Ir a inicio</a>
+           </div>
+       </article>
+   </section>
+
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.frontend.app')
+@section('content')
+
+      <section class="error">
+       <article class="limit">
+           <h4 class="error__title">
+               Error
+               <strong>500</strong>
+           </h4>
+           <strong class="error__subtitle"> Error interno </strong>
+           <p class="error__text">Regresa a la página de inicio</p>
+           <div class="g-button-group">
+               <a href="/" class="g-button m--mini m--211 m--rojo">Ir a inicio</a>
+           </div>
+       </article>
+   </section>
+
+@endsection
