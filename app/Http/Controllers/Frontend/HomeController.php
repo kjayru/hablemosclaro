4fc35120c5cd2ee1f2cbea06d7a7692c550089a1
kjayru <wiltinoco@gmail.com>
2024-09-05 01:57:10 -0500

    fix contador

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -1156,7 +1156,7 @@ class HomeController extends Controller
         // };
 
 
-        $categoria = Category::where('id',7)->first();
+        $categoria = Category::where('id',8)->first();
         // $tags = Tag::all();
         // $autores = Author::all();
         // $tipos = PostType::all();
